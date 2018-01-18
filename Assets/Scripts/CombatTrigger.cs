939d8baee653c223b184f7471ca26a7883a4f4d7
mikebram <mb1620@york.ac.uk>
2018-01-18 20:19:49 +0000

    Enemy AI implemented

---
+++ Assets/Scripts/CombatTrigger.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CombatTrigger : MonoBehaviour {
+
+
+    // Use this for initialization
+    public void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.gameObject.name == "MainPlayer")
+        {
+            SceneManager.LoadScene("unity");
+        }
+    }
+}
+
---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+
+    public float speed;
+    public float displacement;
+
+    private Transform target;
+
+    // Use this for initialization
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Vector2.Distance(transform.position, target.position) > displacement)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        }
+
+
+    }
+}
