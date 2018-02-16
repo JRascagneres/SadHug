007eda413dc612ef622f2b5fd06720ae14ce6363
MarkoMarsenic <mm1660@york.ac.uk>
2018-02-16 03:36:12 +0000

    minigame added (still needs hp bar and score counter)

---
+++ Assets/Minigame/GUItext.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GUItext : MonoBehaviour {
+    public GUIText scoreText;
+    public float score;
+
+    Player_minigame p;
+	// Use this for initialization
+	void Start () {
+        score = 0;
+        UpdateScore();
+	}
+	
+	// Update is called once per frame
+	void UpdateScore () {
+        scoreText.text = "Score: " + score;
+
+	}
+    public void AddScore(float newScoreValue)
+    {
+        score += newScoreValue;
+        UpdateScore();
+    }
+
+    public void TimeAlive()
+    {
+        if (p.GetComponent<Player_minigame>().curHP > 0)
+        {
+            score += Time.deltaTime;
+            AddScore(score);
+        }
+    }
+}
---
+++ Assets/Minigame/HealthBar.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour {
+    public Image currentHealthBar;
+    public Text ratioText;
+    public HealthBar hpBar;
+    private float hitpoint = 100;
+    private float maxHP = 100;
+
+    public Player_minigame p;
+
+	// Use this for initialization
+	void Start () {
+        hpBar = gameObject.GetComponent<HealthBar>();
+        
+	}
+	
+	// Update is called once per frame
+	void UpdateHealthBar () {
+        float ratio = hitpoint / maxHP;
+        currentHealthBar.rectTransform.localScale = new Vector3(ratio, 1, 1);
+        ratioText.text = (ratio * 100).ToString("0") + '%';
+		
+	}
+
+    public void TakeDamage()
+    {
+        if (p.GetComponent<Player_minigame>().wasHit == true)
+        {
+            hitpoint -= 1;
+            p.GetComponent<Player_minigame>().wasHit = false;
+        }
+        
+        if (hitpoint <= 0)
+        {
+            hitpoint = 0;
+        }
+        
+
+        UpdateHealthBar();
+           
+    }
+}
