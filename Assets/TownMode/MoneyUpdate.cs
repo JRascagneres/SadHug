a9533646b358261da7a4323abfb342102153d9e5
Jacques Racagneres <jacquesrascagneres@gmail.com>
2018-02-18 23:25:51 +0000

    Money shown in TownMode

---
+++ Assets/TownMode/MoneyUpdate.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+//-- This whole class was added for Assessment 3
+
+/// <summary>
+/// Simply updates money shown in town mode
+/// </summary>
+public class MoneyUpdate : MonoBehaviour {
+
+    /// <summary>
+    /// Ensures money is kept up to date in town mode
+    /// </summary>
+    void Update()
+    {
+        Text moneyText = gameObject.GetComponent<Text>();
+        moneyText.text = "Current Money: " + PlayerData.instance.data.Money;
+    }
+}
